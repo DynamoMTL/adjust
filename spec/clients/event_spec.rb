ab67968b78ebece6bb7d2c88c5a7e8a085119db5
Hugo Bastien <hugobast@gmail.com>
2015-05-05 23:00:26 -0400

    Cleans up the event_spec

--- spec/clients/event_spec.rb
+++ spec/clients/event_spec.rb
@@ -5,36 +5,41 @@ module Adjust
     describe Event do
       let(:idfa) { ENV['ADJUST_TEST_IDFA'] }
       let(:app_token) { ENV['ADJUST_APP_TOKEN'] }
+      let(:endpoint) { 'https://s2s.adjust.com/event' }
 
-      subject(:event) do
-        Event.new(
-          idfa: idfa,
-          app_token: app_token,
-          event_token: 'tawmb9',
-          created_at: Time.now.iso8601,
-          environment: :sandbox
-        )
+      subject(:event) { Event.new(**event_params) }
+
+      before do
+        stub_request(:post, endpoint)
+          .to_return(body: '{"status":"OK"}')
       end
 
-      it "has a s2s value that's always equal to 1" do
-        expect(event.s2s).to be 1
-      end
-
-      describe '#track!', vcr: { cassette_name: :event_success } do
+      describe '#track!' do
         subject(:track!) { event.track! }
 
         it 'makes a post request to adjust' do
           track!
 
-          expect(post_request).to have_been_made
+          expect(a_post_request).to have_been_made
         end
 
-        it 'updates event with response tracker token' do
+        it 'updates event with status returned' do
           expect(track!).to be_success
         end
 
-        def post_request
-          a_request :post, 'https://s2s.adjust.com/event'
+        def a_post_request
+          a_request(:post, endpoint)
+            .with(body: hash_including(event_params))
+        end
+
+        def event_params
+          {
+            idfa: idfa,
+            app_token:  app_token,
+            event_token: 'tawmb9',
+            created_at: Time.now.iso8601,
+            environment: 'sandbox'
+          }
         end
       end
     end
