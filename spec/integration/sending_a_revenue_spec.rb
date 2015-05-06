4ca7f38d6b5c0fa58d673baf3b561ceca78288cf
Hugo Bastien <hugobast@gmail.com>
2015-05-05 23:52:26 -0400

    Delinting

--- spec/integration/sending_a_revenue_spec.rb
+++ spec/integration/sending_a_revenue_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe 'Sending an event' do
   context 'with success', vcr: { cassette_name: :revenue_success }  do
-    subject do Adjust.revenue \
-      token: 'tawmb9',
-      revenue: 2.0,
-      currency: 'CAD',
-      idfa: ENV['ADJUST_TEST_IDFA']
+    subject do
+      Adjust.revenue \
+        token: 'tawmb9',
+        revenue: 2.0,
+        currency: 'CAD',
+        idfa: ENV['ADJUST_TEST_IDFA']
     end
 
     it 'succeeds' do
